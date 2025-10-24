3c0efe417331c0a850d7576e9e4c9f1c53de5222
td1357 <td_1357@yahoo.com>
2025-10-24 13:25:34 -0400

    Add multiply and divide functions

--- math_tool.py
+++ math_tool.py
@@ -5,6 +5,14 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
+
 if __name__ == "__main__":
     print("Addition:", add(5, 3))
     print("Subtraction:", subtract(5, 3))
+    print("multiplication:", multiplication(5, 3))
+    print("isionion:", division(5, 3))
