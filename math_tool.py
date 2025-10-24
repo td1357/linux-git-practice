5a6d380c2aba861d86503119166cee450e341124
td1357 <td_1357@yahoo.com>
2025-10-24 19:18:46 -0400

    Fix subtraction bug

--- math_tool.py
+++ math_tool.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     print("Addition:", add(5, 3))
     print("Subtraction:", subtract(5, 3))
     print("Multiplication:", multiplication(5, 3))
-    print("Disionion:", division(5, 3))
+    print("Division:", division(5, 3))
