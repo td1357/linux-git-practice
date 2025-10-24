d5a821bc92b5075590382309d7cc6e1939eb7706
td1357 <td_1357@yahoo.com>
2025-10-24 13:30:10 -0400

    modified

--- math_tool.py
+++ math_tool.py
@@ -14,5 +14,5 @@ def division(a, b):
 if __name__ == "__main__":
     print("Addition:", add(5, 3))
     print("Subtraction:", subtract(5, 3))
-    print("multiplication:", multiplication(5, 3))
-    print("isionion:", division(5, 3))
+    print("Multiplication:", multiplication(5, 3))
+    print("Disionion:", division(5, 3))
